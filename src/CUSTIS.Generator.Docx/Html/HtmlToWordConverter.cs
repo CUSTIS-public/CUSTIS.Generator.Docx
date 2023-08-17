b66a83e94be98313491ee8e613d25c088e747acc
Dmitry Razumikhin <dmitry.razumikhin@gmail.com>
2023-08-17 18:16:12 +0300

    Extract all model types to the separate file

--- src/CUSTIS.Generator.Docx/Html/HtmlToWordConverter.cs
+++ src/CUSTIS.Generator.Docx/Html/HtmlToWordConverter.cs
@@ -148,51 +148,6 @@ public static class HtmlToWordConverter
         }
     }
 
-    public interface IToken
-    {
-    }
-
-    public abstract class TagToken : IToken
-    {
-        protected TagToken(string name)
-        {
-            Name = name;
-        }
-
-        public string Name { get; }
-    }
-
-    public class OpenTagToken : TagToken
-    {
-        public OpenTagToken(string name) : base(name)
-        {
-        }
-    }
-
-    public class CloseTagToken : TagToken
-    {
-        public CloseTagToken(string name) : base(name)
-        {
-        }
-    }
-
-    public sealed class WhiteSpaceToken : TextToken
-    {
-        public WhiteSpaceToken(string value) : base(value)
-        {
-        }
-    }
-
-    public class TextToken : IToken
-    {
-        public TextToken(string value)
-        {
-            Value = value;
-        }
-
-        public string Value { get; }
-    }
-
     private static void AppendParagraph(IList<Paragraph> paragraphs, StringBuilder current, ListInfo? currentList)
     {
         var text = current.ToString().Trim();
---
+++ src/CUSTIS.Generator.Docx/Html/Models.cs
@@ -0,0 +1,46 @@
+﻿namespace CUSTIS.Generator.Docx.Html;
+
+public interface IToken
+{
+}
+
+public abstract class TagToken : IToken
+{
+    protected TagToken(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+}
+
+public class OpenTagToken : TagToken
+{
+    public OpenTagToken(string name) : base(name)
+    {
+    }
+}
+
+public class CloseTagToken : TagToken
+{
+    public CloseTagToken(string name) : base(name)
+    {
+    }
+}
+
+public class TextToken : IToken
+{
+    public TextToken(string value)
+    {
+        Value = value;
+    }
+
+    public string Value { get; }
+}
+
+public sealed class WhiteSpaceToken : TextToken
+{
+    public WhiteSpaceToken(string value) : base(value)
+    {
+    }
+}
